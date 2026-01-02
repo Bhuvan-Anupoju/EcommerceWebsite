582d7660615f8ca13f2e64b218fd7ed74aa0ccfd
foodyhouse <foodyhouse05@gmail.com>
2026-01-02 17:05:28 +0530

    generate html for the cart item

--- React-frontend/src/App.jsx
+++ React-frontend/src/App.jsx
@@ -11,7 +11,7 @@ import { useEffect, useState } from "react";
 function App() {
   const [cart, setCart] = useState([]);
   useEffect(() => {
-    axios.get("/api/cart-items").then((res) => {
+    axios.get("/api/cart-items?expand=product").then((res) => {
       setCart(res.data);
     });
   }, []);
