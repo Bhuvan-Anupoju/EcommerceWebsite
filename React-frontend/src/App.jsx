9f034883d422dada0af0190464b21c5405ed8ce3
foodyhouse <foodyhouse05@gmail.com>
2026-01-04 12:33:50 +0530

    create add to cart feature

--- React-frontend/src/App.jsx
+++ React-frontend/src/App.jsx
@@ -10,17 +10,18 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [cart, setCart] = useState([]);
+  const loadCart = async () => {
+    const response = await axios.get("/api/cart-items?expand=product");
+    setCart(response.data);
+  };
   useEffect(() => {
-    const getAppData = async () => {
-      const response = await axios.get("/api/cart-items?expand=product");
-      setCart(response.data);
-    };
-    getAppData();
+    loadCart(); // uses old state if function changes
+    /// eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <>
       <Routes>
-        <Route index element={<HomePage cart={cart} />} />
+        <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
         <Route path="checkout" element={<CheckoutPage cart={cart} />} />
         <Route path="orders" element={<OrdersPage cart={cart} />} />
         <Route
--- React-frontend/src/pages/home/HomePage.jsx
+++ React-frontend/src/pages/home/HomePage.jsx
@@ -7,7 +7,7 @@ import { ProductsGrid } from "./ProductsGrid";
 
 import { useEffect, useState } from "react";
 
-export function HomePage({ cart }) {
+export function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -25,7 +25,7 @@ export function HomePage({ cart }) {
       <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
       <Header cart={cart} />
       <div className="home-page">
-        <ProductsGrid products={products} />
+        <ProductsGrid products={products} loadCart={loadCart} />
       </div>
     </>
   );
