783dfe7ed979460f061ec973b8ca1dd254548ea6
foodyhouse <foodyhouse05@gmail.com>
2026-01-12 14:38:42 +0530

    changed to amazon

--- React-frontend/src/pages/checkout/CheckoutPage.jsx
+++ React-frontend/src/pages/checkout/CheckoutPage.jsx
@@ -38,8 +38,11 @@ export function CheckoutPage({ cart, loadCart }) {
         <div className="header-content">
           <div className="checkout-header-left-section">
             <Link to="/">
-              <img className="logo" src="images/logo.png" />
-              <img className="mobile-logo" src="images/mobile-logo.png" />
+              <img className="logo" src="images/amazon-logo.png" />
+              <img
+                className="mobile-logo"
+                src="images/amazon-mobile-logo.png"
+              />
             </Link>
           </div>
 
